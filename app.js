16cd77236ca754ef8830a1714e2669714020c8ed
tarung10 <tarun08007@gmail.com>
2020-08-28 15:59:27 +0530

    Updated

--- app.js
+++ app.js
@@ -8,68 +8,68 @@ app.use(express.urlencoded({extended: true}));
 // Determining OS and req file
 let file = '/etc/hosts';
 if (process.platform === 'win32') 
-	 file = 'C://Windows//System32//drivers//etc';
+  file = 'C://Windows//System32//drivers//etc';
 let websites = [];
 
 // Routing rules
 app.get('/', (req, res) => {
-	fs.readFile('./templates/default-template.html', 'utf-8', (err, data) =>{
-		if (err) return res.status(400).send('Error in opening <i>default-template.html</i>...');
-		withData(data, res);
-	});
+  fs.readFile('./templates/default-template.html', 'utf-8', (err, data) =>{
+    if (err) return res.status(400).send('Error in opening <i>default-template.html</i>...');
+    withData(data, res);
+  });
 });
 
 app.get('/add', (req, res) => {
-	fs.readFile('./templates/add-template.html', 'utf-8', (err, data) => {
-		if (err) return res.status(404).send('Error in opening <emp>add-template<emp> file...');
-		res.send(data);
-	});
+  fs.readFile('./templates/add-template.html', 'utf-8', (err, data) => {
+    if (err) return res.status(404).send('Error in opening <emp>add-template<emp> file...');
+    res.send(data);
+  });
 });
 
 app.post('/add', (req, res) => {
-	const index = req.body.website.indexOf('.');
-	const length = req.body.website.length;
-	if (index === -1 || index === 0 || index > (length - 3)) return res.status(400).send('<h1>Invalid website...</h1>');
+  const index = req.body.website.indexOf('.');
+  const length = req.body.website.length;
+  if (index === -1 || index === 0 || index > (length - 3)) return res.status(400).send('<h1>Invalid website...</h1>');
 
-	const result = '0.0.0.0  ' + req.body.website + '\n';
-	fs.appendFileSync(file, result, 'utf-8', err => res.status(400).send('Error in updating...'));
-	res.redirect('/');
+  const result = '0.0.0.0  ' + req.body.website.toLowerCase() + '\n';
+  fs.appendFileSync(file, result, 'utf-8', err => res.status(400).send('Error in updating...'));
+  res.redirect('/');
 });
 
 app.post('/delete', (req, res) => {
-	let sites = req.body.sites;
-	let result = '';
+  let sites = req.body.sites;
+  let result = '127.0.0.1  localhost\n';
 
-	websites = websites.filter(x => !sites.includes(x));
-	for(let id = 0; id < websites.length; id++)
-		result += '0.0.0.0  ' + websites[id] + '\n';
+  websites = websites.filter(x => !sites.includes(x));
+  for(let id = 0; id < websites.length; id++)
+    result += '0.0.0.0  ' + websites[id] + '\n';
 
-	fs.writeFile(file, result, 'utf-8', err => console.log('Error : ', err));
-	websites = [];
-	sites = '';
-	res.redirect('/');
+  fs.writeFile(file, result, 'utf-8', err => console.log('Error : ', err));
+  websites = [];
+  sites = '';
+  res.redirect('/');
 });
 
 function withData(oldHTML, res) {
-	let result = '';
-	let template = '<tr><td><website></td><td><input type="checkbox" name="sites" value="<id>" class="form-control"></td></tr>';
-	websites = [];
+  let result = '';
+  let template = '<tr><td><website></td><td><input type="checkbox" name="sites" value="<id>"></td></tr>';
+  websites = [];
 
-	fs.readFile(file, 'utf-8', (err, data) => {
-		if (err) throw err;
+  fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) throw err;
 
-		const lines = data.split('\n').filter(line => line.includes('localhost') || line.includes('127.0.0.1') || line.includes('0.0.0.0'));
-		for(let line = 0; line < lines.length; line++) {
-			websites.push(lines[line].split(' ')[2]);
+    const lines = data.split('\n').filter(line => !line.includes('localhost') && ( line.includes('127.0.0.1') || line.includes('0.0.0.0') ));
+    for(let line = 0; line < lines.length; line++) {
+      websites.push(lines[line].split(' ')[2]);
 
-			let temp = template;
-			temp = temp.replace('<website>', lines[line].split(' ')[2]);
-			temp = temp.replace('<id>', lines[line].split(' ')[2]);
-			result += temp;
-		}
-		result = oldHTML.replace('{%RESULT%}', result);
-		return res.send(result);
-	});
+      let temp = template;
+      temp = temp.replace('<website>', lines[line].split(' ')[2]);
+      temp = temp.replace('<id>', lines[line].split(' ')[2]);
+      result += temp;
+    }
+    result = oldHTML.replace('{%RESULT%}', result);
+    return res.send(result);
+  });
 }
 
-app.listen(3000, () => console.log('Listening on 3000 port...'));
+app.listen(3000, () => console.log('Open http://127.0.0.1:3000 in your favourite browser...'));
